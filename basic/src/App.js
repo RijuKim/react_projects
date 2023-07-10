678609cc4ecac06fc5ef01bb12b469bbbc432fb4
Rijoo <rijoo1121@sookmyung.ac.kr>
2023-07-10 22:25:18 +0900

    Add: 리액트 기초 구조 및 App.js 연습

--- basic/src/App.js
+++ basic/src/App.js
@@ -2,23 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = '리주'
+  const list = ['우유', '딸기', '바나나', '요거트']
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className='orange'>{`Hello! ${name}`}</h1>
+      <h2>Hello!</h2>
+      <p>{name}</p>
+      <ul>
+        {list.map((item) => (
+          <li>{item}</li>
+        ))}
+      </ul>
+      <img
+          style = {{width: '200px', height: '200px'}}
+          src='https://images.unsplash.com/photo-1671362500619-4b9c06b5c5e1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDExfGJvOGpRS1RhRTBZfHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=500&q=60'
+          alt='nature'
+      />
+    </>
   );
 }
 
