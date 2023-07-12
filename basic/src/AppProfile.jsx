6287540a4b5bdba0b26771fdb0809c6037b880fc
Rijoo <rijoo1121@sookmyung.ac.kr>
2023-07-12 23:05:29 +0900

    Update: Profile 컴포넌트, props 기능 구현

--- basic/src/AppProfile.jsx
+++ basic/src/AppProfile.jsx
@@ -4,7 +4,21 @@ import Profile from './components/Profile';
 function AppProfile() {
   return (
     <>
-      <Profile/>
+      <Profile 
+        image='https://images.unsplash.com/photo-1688932112116-1811e7717379?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNjB8fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=600&q=60'
+        name='Rijoo Kim' 
+        title='프론트엔드 개발자'
+      />
+      <Profile
+        image='https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGVvcGxlfGVufDB8fDB8fHww&auto=format&fit=crop&w=600&q=60'
+        name='Anna Young' 
+        title='백엔드 개발자'
+      />
+      <Profile
+        image='https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cGVvcGxlfGVufDB8fDB8fHww&auto=format&fit=crop&w=600&q=60'
+        name='Bob Yu' 
+        title='프론트엔드 개발자'      
+      />
     </>
   );
 }
--- basic/src/components/Profile.jsx
+++ basic/src/components/Profile.jsx
@@ -1,3 +1,15 @@
-export default function Profile() {
-    return <h1>Profile</h1>
+import React from "react";
+
+export default function Profile({image, name, title}) {
+  return (
+    <div className="profile">
+      <img
+        className="photo"
+        src={image}
+        alt="avater"
+      />
+      <h1>{name}</h1>
+      <p>{title}</p>
+    </div>
+  );
 }
